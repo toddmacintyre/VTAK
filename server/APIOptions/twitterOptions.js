8e4db8e0fbeab67d2520ef97921825475bd2c2fd
iamongit <ayousuf2@binghamton.edu>
2017-03-10 16:22:17 -0500

    add twitter API controller, twitter helper function

---
+++ server/APIOptions/twitterOptions.js
@@ -0,0 +1,22 @@
+
+
+module.exports ={
+options: { method: 'GET',
+  url: 'https://api.twitter.com/1.1/statuses/user_timeline.json',
+  qs: 
+   { oauth_consumer_key: 'mqWa8AeQ946Xu06JQCVcgdg22',
+     oauth_token: '94060681-W6UyJxbwIVN57jygEu5fdOiLdJfnJ9PbJbJNvqUW2',
+     oauth_signature_method: 'HMAC-SHA1',
+     oauth_timestamp: '1489173524',
+     oauth_nonce: 'prwE4q',
+     oauth_version: '1.0',
+     oauth_signature: 'WSDQ2MjQzkpfMBwOsgau7bVhIAc=',
+     screen_name: 'jpackel',
+     count: '20' },
+  headers: 
+   {'access-token':'94060681-W6UyJxbwIVN57jygEu5fdOiLdJfnJ9PbJbJNvqUW2',
+    'access-token-secret': 'uti3oZX7LmgaFHsea8Sk64acoW7gd4NrVBSbe8E6nmhm0',
+   	'postman-token': 'b8538c8f-a604-f5ec-32d3-20aa4de2e51c',
+     'cache-control': 'no-cache' } 
+  };
+}
