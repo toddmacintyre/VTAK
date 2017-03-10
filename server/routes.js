b74bbc9426c6fea08a87d5d353fdff25097858b1
iamongit <ayousuf2@binghamton.edu>
2017-03-10 17:42:24 -0500

    router file started

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,5 @@
-module.exports = function(app, express) {
-  
-}
+var twitterController = require('../APIOptions');
+var watsonController = require('/');
+
+var router = require('express').Router();
+router.get('/api/handle', twitterController.getRequestTwitter);
