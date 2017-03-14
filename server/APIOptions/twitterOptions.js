7d69a414f63409bb721bc36f878fe7a3521f8952
iamongit <ayousuf2@binghamton.edu>
2017-03-14 17:23:01 -0400

    twitter api working, watson working, check though

--- server/APIOptions/twitterOptions.js
+++ server/APIOptions/twitterOptions.js
@@ -1,23 +1,19 @@
 // var env = require('dotenv'); // will use this later but not working now so require('./.env') for now
 var envVars = require('../../env.json');
 
+var bearer_token = {"token_type":"bearer","access_token":"AAAAAAAAAAAAAAAAAAAAAN0yzgAAAAAAuA1ty4YD%2BOQX6jCnVa%2Fwk%2FnfNhI%3DpF9GEBB3hTF617KlRVcJFPX8naMLQZBqgD2tiDTU2zbOJGhfCW"}
 module.exports = {
-options: { method: 'GET',
-  url: 'https://api.twitter.com/1.1/statuses/user_timeline.json',
-  qs:
-   { oauth_consumer_key: 'mqWa8AeQ946Xu06JQCVcgdg22',
-     oauth_token: '94060681-W6UyJxbwIVN57jygEu5fdOiLdJfnJ9PbJbJNvqUW2',
-     oauth_signature_method: 'HMAC-SHA1',
-     oauth_timestamp: '1489173524',
-     oauth_nonce: 'prwE4q',
-     oauth_version: '1.0',
-     oauth_signature: 'WSDQ2MjQzkpfMBwOsgau7bVhIAc=',
-     screen_name: 'jpackel',
-     count: '20' },
-  headers:
-   {'access-token': envVars.twitter_access_token,
-    'access-token-secret': envVars.twitter_access_token_secret,
-   	'postman-token': 'b8538c8f-a604-f5ec-32d3-20aa4de2e51c',
-    'cache-control': 'no-cache' }
-  }
+    options: {
+        method: 'GET',
+        url: 'https://api.twitter.com/1.1/statuses/user_timeline.json',
+        qs: {
+            screen_name: "jpackel",
+            count:'30'
+
+        },
+        json: true,
+        headers: {
+            "Authorization": "Bearer " + bearer_token["access_token"]
+        }
+    }
 }
--- server/controllers/twitterApiController.js
+++ server/controllers/twitterApiController.js
@@ -1,27 +1,55 @@
 var morgan = require('morgan');
 var request = require('request');
 
+var consumer_key = 'mqWa8AeQ946Xu06JQCVcgdg22';
+var consumer_secret = 'saIMEFMAKP8PBSyeBxiDcV7GBhcsUPfyDoqYwTbtI8jguEIi14';
+var encode_secret = new Buffer(consumer_key + ':' + consumer_secret).toString('base64');
+
+
 
 module.exports = {
-	getRequestTwitter: function(optionsTwitter, handle, callback) {
-		optionsTwitter.options.qs['screen_name'] = handle;
-		request(optionsTwitter.options, function(error, response, body) {
-			if(error) {
-				console.log(error, 'error in twitter API get request');
-				// throw new Error(error);
-			} else {
-				var responseObject = JSON.parse(body);
-				var finalString =  tweetParser(responseObject)
-				callback(error, finalString);
-			}
-		})
-	}
+    getRequestTwitter: function(optionsTwitter, handle, callback) {
+        // var encode_secret = new Buffer(consumer_key + ':' + consumer_secret).toString('base64');
+        // var options2 = {
+        //     url: 'https://api.twitter.com/oauth2/token',
+        //     headers: {
+        //         'Authorization': 'Basic ' + encode_secret,
+        //         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        //     },
+        //     body: 'grant_type=client_credentials'
+        // };
+
+        // request.post(options2, function(error, response, body) {
+        //     console.log(body, "BEARERRR TOKENNNNNN"); // <<<< This is your BEARER TOKEN !!!
+        //     // optionsTwitter.options.headers['Authorization'] = body;
+        // });  
+
+        //** This request above was to get bearer token, we needed to run it only once **//
+        //** Didn't delete it as we might need it again **//
+
+        optionsTwitter.options.qs['screen_name'] = handle;
+
+        request(optionsTwitter.options, function(error, response, body) {
+            if (error) {
+                console.log(error, 'error in twitter API get request');
+                // throw new Error(error);
+            } else {
+            	// console.log(body, "herioioioioioio")
+                // var responseObject = JSON.parse(body);
+                var responseObject = body;
+                var finalString = tweetParser(responseObject)
+                console.log(finalString, "FINAL STRININININIGIGIGI")
+                callback(error, finalString);
+            }
+        })
+    }
 };
 
 var tweetParser = function(twitterResponseArray) {
-	var stringForWatson = '';
-	twitterResponseArray.forEach(function(tweetObject) {
-	stringForWatson += tweetObject.text+". ";
-	})
-	return stringForWatson;
+    var stringForWatson = '';
+    // console.log(twitterResponseArray, "RESP ARRAYAYAYAY")
+    twitterResponseArray.forEach(function(tweetObject) {
+        stringForWatson += tweetObject.text + ". ";
+    })
+    return stringForWatson;
 };
